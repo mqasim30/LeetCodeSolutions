9a56da34f980065523d71523f769bd6c7426b3bf
mqasim30 <qasimcheema30@gmail.com>
2024-05-31 14:52:07 +0500

    -Added New Problem Solution.

---
+++ SingleNumberIII.cs
@@ -0,0 +1,41 @@
+namespace LeetCode.SingleNumberIII
+{
+    public class Solution
+    {
+        public int[] SingleNumber(int[] nums)
+        {
+            int xorResult = 0;
+            foreach (int num in nums)
+            {
+                xorResult ^= num;
+            }
+
+            int diffBit = xorResult & -xorResult;
+
+            int[] result = new int[2];
+            foreach (int num in nums)
+            {
+                if ((num & diffBit) == 0)
+                {
+                    result[0] ^= num;
+                }
+                else
+                {
+                    result[1] ^= num;
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int[] nums = [1, 2, 1, 3, 2, 5];
+            solution.SingleNumber(nums);
+        }
+    }
+}
