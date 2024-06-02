b684118b6b84fc6d6d7a575c6a00da5ffba5fdf0
mqasim30 <qasimcheema30@gmail.com>
2024-06-02 19:43:23 +0500

    -Added New Problem Solution.

---
+++ ReverseString.cs
@@ -0,0 +1,20 @@
+namespace LeetCode.ReverseString
+{
+    public class Solution
+    {
+        public void ReverseString(char[] s)
+        {
+            Array.Reverse(s);
+        }
+    }
+
+    // public class Program
+    // {
+    //     public static void Main(string[] args)
+    //     {
+    //         Solution solution = new Solution();
+    //         char[] s = ['h','e','l','l','o'];
+    //         solution.ReverseString(s);
+    //     }
+    // }
+}
