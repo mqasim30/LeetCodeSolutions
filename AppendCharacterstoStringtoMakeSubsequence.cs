f3579e60d1746dc8850b546b93eddbdce555807f
mqasim30 <qasimcheema30@gmail.com>
2024-06-03 18:40:51 +0500

    -Added New Problem Solution.

---
+++ AppendCharacterstoStringtoMakeSubsequence.cs
@@ -0,0 +1,31 @@
+using System.Drawing;
+
+namespace LeetCode.AppendCharacterstoStringtoMakeSubsequence
+{
+    public class Solution
+    {
+        public int AppendCharacters(string s, string t)
+        {
+            int pt = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (pt < t.Length && t[pt] == s[i])
+                {
+                    pt += 1;
+                }
+            }
+            return t.Length - pt;
+        }
+    }
+    // public class Program
+    // {
+    //     public static void Main(string[] args)
+    //     {
+    //         Solution solution = new Solution();
+    //         string s = "coaching";
+    //         string t = "coding";
+    //         Console.WriteLine(solution.AppendCharacters(s, t));
+    //     }
+    // }
+}
