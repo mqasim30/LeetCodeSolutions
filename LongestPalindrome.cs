8db513483a0d11b0323b165572c5f8a011ce8577
mqasim30 <qasimcheema30@gmail.com>
2024-06-04 14:41:51 +0500

    -Added New Problem Solution.

---
+++ LongestPalindrome.cs
@@ -0,0 +1,44 @@
+namespace LeetCode.LongestPalindrome
+{
+    public class Solution
+    {
+        public int LongestPalindrome(string s)
+        {
+            Dictionary<char, int> keyValuePairs = new Dictionary<char, int>();
+            int result = 0;
+            foreach (char c in s)
+            {
+                if (keyValuePairs.ContainsKey(c))
+                {
+                    keyValuePairs[c] += 1;
+                }
+                else
+                {
+                    keyValuePairs[c] = 1;
+                }
+                if (keyValuePairs[c] % 2 == 0)
+                {
+                    result += 2;
+                }
+            }
+            foreach (var item in keyValuePairs.Values)
+            {
+                if (item % 2 == 1)
+                {
+                    result += 1;
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            string s = "abccccdd";
+            Console.WriteLine(solution.LongestPalindrome(s));
+        }
+    }
+}
