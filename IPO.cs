4f129c970b6e9aa5ac7c40567b5e15970195ba4b
Muhammad Qasim <qasimcheema30@gmail.com>
2024-06-15 20:36:41 +0500

    -Added New Problem Solution.

--- IPO.cs
+++ IPO.cs
@@ -7,36 +7,29 @@ public class Solution
         int n = profits.Length;
         List<(int,int)> projects = new List<(int,int)>();
 
-        // Creating list of projects with capital and profits
         for (int i = 0; i < n; i++)
         {
             projects.Add(new (capital[i], profits[i]));
         }
 
-        // Sorting projects by capital required
         projects.Sort((a, b) => a.Item1 - b.Item2);
 
-        // Max-heap to store profits
         PriorityQueue<int, int> maxHeap = new PriorityQueue<int, int>(Comparer<int>.Create((a, b) => b - a));
         int currentIndex = 0;
 
-        // Main loop to select up to k projects
         for (int j = 0; j < k; j++)
         {
-            // Add all profitable projects that we can afford
             while (currentIndex < n && projects[currentIndex].Item1 <= w)
             {
                 maxHeap.Enqueue(projects[currentIndex].Item2, projects[currentIndex].Item2);
                 currentIndex++;
             }
 
-            // If no projects can be funded, break out of the loop
             if (maxHeap.Count == 0)
             {
                 break;
             }
 
-            // Otherwise, take the project with the maximum profit
             w += maxHeap.Dequeue();
         }
 
@@ -52,6 +45,6 @@ public class Program
         int k = 2, w = 0;
         int[] profits = { 1, 2, 3 };
         int[] capital = { 0, 1, 1 };
-        Console.WriteLine(solution.FindMaximizedCapital(k, w, profits, capital));  // Output should be 4
+        Console.WriteLine(solution.FindMaximizedCapital(k, w, profits, capital));
     }
 }
