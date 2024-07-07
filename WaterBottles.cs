817adcbec252cb37743b7acf9ebe62a4b3011e15
Muhammad Qasim <qasimcheema30@gmail.com>
2024-07-07 20:32:06 +0500

    -Added New Problem Solution.

---
+++ WaterBottles.cs
@@ -0,0 +1,9 @@
+namespace LeetCode.WaterBottle;
+
+public class Solution
+{
+    public int NumWaterBottles(int numBottles, int numExchange)
+    {
+        return numBottles + (numBottles - 1) / (numExchange - 1);
+    }
+}
