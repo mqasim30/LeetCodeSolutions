f927455b0d27f92124fd4934a6715b096759cc2e
Muhammad Qasim <qasimcheema30@gmail.com>
2024-07-10 13:24:01 +0500

    -Added New Problem Solution.

---
+++ CrawlerLogFolder.cs
@@ -0,0 +1,36 @@
+namespace LeetCode.CrawlerLogFolder;
+
+public class Solution
+{
+    public int MinOperations(string[] logs)
+    {
+        Stack<string> pathsStack = new Stack<string>();
+
+        foreach (string log in logs)
+        {
+            if (log == "../")
+            {
+                if (pathsStack.Count > 0)
+                {
+                    pathsStack.Pop();
+                }
+            }
+            else if (log != "./")
+            {
+                pathsStack.Push(log);
+            }
+        }
+
+        return pathsStack.Count;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string[] logs = ["d1/", "d2/", "../", "d21/", "./"];
+        Console.WriteLine(solution.MinOperations(logs));
+    }
+}
