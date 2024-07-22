f5747a74f4e71bf1e02faa4ee5c3f9d611b0c765
Muhammad Qasim <qasimcheema30@gmail.com>
2024-07-22 20:32:44 +0500

    -Added New Problem Solution.

---
+++ SortthePeople.cs
@@ -0,0 +1,21 @@
+namespace LeetCode.SortthePeople;
+
+public class Solution
+{
+    public string[] SortPeople(string[] names, int[] heights)
+    {
+        Array.Sort(heights, names, Comparer<int>.Create((x, y) => y.CompareTo(x)));
+        return names;
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+        string[] names = ["Mary", "John", "Emma"];
+        int[] heights = [180, 165, 170];
+        Console.WriteLine(solution.SortPeople(names, heights));
+    }
+}
