7919a2e8ab092cbb4ae4a01d9af6a1b90edcc3af
Muhammad Qasim <qasimcheema30@gmail.com>
2024-08-05 18:04:19 +0500

    -Added New Problem Solution.

---
+++ KthDistinctStringinanArray.cs
@@ -0,0 +1,33 @@
+namespace LeetCode.KthDistinctStringinanArray;
+public class Solution
+{
+    public string KthDistinct(string[] arr, int k)
+    {
+        Dictionary<string, int> keyValuePairs = new();
+        foreach (var item in arr)
+        {
+            if (!keyValuePairs.TryAdd(item, 1))
+            {
+                keyValuePairs[item]++;
+            }
+        }
+
+        var distinctElements = keyValuePairs
+                                .Where(kv => kv.Value == 1)
+                                .Select(kv => kv.Key)
+                                .ToList();
+
+        return k > distinctElements.Count ? "" : distinctElements[k - 1];
+    }
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Solution solution = new();
+        string[] arr = ["d", "b", "c", "b", "c", "a"];
+        int k = 2;
+        Console.WriteLine(solution.KthDistinct(arr, k));
+    }
+}
