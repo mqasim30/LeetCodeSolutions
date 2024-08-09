9d53a6ef6ae44026d95bb916461ce5a5bda89141
Muhammad Qasim <qasimcheema30@gmail.com>
2024-08-09 17:27:30 +0500

    -Added New Problem Solution.

--- MagicSquaresInGrid.cs
+++ MagicSquaresInGrid.cs
@@ -1,61 +1,65 @@
 namespace LeetCode.MagicSquaresInGrid;
-
 public class Solution
 {
     public int NumMagicSquaresInside(int[][] grid)
     {
-        bool IsMagic(int i, int j)
+        int rowCount = grid.Length;
+        int colCount = grid[0].Length;
+        if (rowCount < 3 || colCount < 3) return 0;
+
+        int magicSquareCount = 0;
+
+        for (int i = 1; i < rowCount - 1; i++)
         {
-            bool[] once = new bool[10];
-            int[] rowSum = new int[3];
-            int[] colSum = new int[3];
-
-            for (int a = i - 1; a <= i + 1; a++)
+            for (int j = 1; j < colCount - 1; j++)
             {
-                for (int b = j - 1; b <= j + 1; b++)
+                if (grid[i][j] == 5 && IsMagicSquare(grid, i, j))
                 {
-                    int x = grid[a][b];
-                    if (x < 1 || x > 9) return false;
-                    rowSum[a - i + 1] += x;
-                    colSum[b - j + 1] += x;
-                    if (once[x]) return false; // it's not unique
-                    once[x] = true;
-                }
-            }
-
-            for (int b = 1; b <= 9; b++)
-            {
-                if (!once[b]) return false;
-            }
-
-            foreach (var sum in rowSum)
-            {
-                if (sum != 15) return false;
-            }
-            foreach (var sum in colSum)
-            {
-                if (sum != 15) return false;
-            }
-
-            return grid[i - 1][j - 1] + grid[i + 1][j + 1] == 10 &&
-                   grid[i + 1][j - 1] + grid[i - 1][j + 1] == 10;
-        }
-
-        int r = grid.Length;
-        int c = grid[0].Length;
-        if (r < 3 || c < 3) return 0;
-
-        int cnt = 0;
-        for (int i = 1; i < r - 1; i++)
-        {
-            for (int j = 1; j < c - 1; j++)
-            {
-                if (grid[i][j] == 5 && IsMagic(i, j))
-                {
-                    cnt++;
+                    magicSquareCount++;
                 }
             }
         }
-        return cnt;
+
+        return magicSquareCount;
+    }
+
+    private bool IsMagicSquare(int[][] grid, int centerX, int centerY)
+    {
+        bool[] seenNumbers = new bool[10];
+        int[] rowSums = new int[3];
+        int[] colSums = new int[3];
+
+        for (int rowOffset = -1; rowOffset <= 1; rowOffset++)
+        {
+            for (int colOffset = -1; colOffset <= 1; colOffset++)
+            {
+                int currentNumber = grid[centerX + rowOffset][centerY + colOffset];
+
+                if (currentNumber < 1 || currentNumber > 9 || seenNumbers[currentNumber])
+                {
+                    return false;
+                }
+
+                seenNumbers[currentNumber] = true;
+                rowSums[rowOffset + 1] += currentNumber;
+                colSums[colOffset + 1] += currentNumber;
+            }
+        }
+
+        // Check if all numbers from 1 to 9 are present and unique
+        for (int i = 1; i <= 9; i++)
+        {
+            if (!seenNumbers[i]) return false;
+        }
+
+        // Check if all rows, columns, and diagonals sum to 15
+        if (rowSums[0] != 15 || rowSums[1] != 15 || rowSums[2] != 15) return false;
+        if (colSums[0] != 15 || colSums[1] != 15 || colSums[2] != 15) return false;
+
+        // Check diagonals
+        if (grid[centerX - 1][centerY - 1] + grid[centerX + 1][centerY + 1] != 10) return false;
+        if (grid[centerX + 1][centerY - 1] + grid[centerX - 1][centerY + 1] != 10) return false;
+
+        return true;
     }
 }
