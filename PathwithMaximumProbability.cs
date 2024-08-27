e48366d7254a41ce15e864ade26dcb662b31415d
Muhammad Qasim <qasimcheema30@gmail.com>
2024-08-27 14:09:14 +0500

    -Added New Problem Solution.

---
+++ PathwithMaximumProbability.cs
@@ -0,0 +1,34 @@
+namespace LeetCode.PathwithMaximumProbability;
+public class Solution
+{
+    public double MaxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node)
+    {
+        double[] maxProb = new double[n];
+        maxProb[start_node] = 1.0;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            bool updated = false;
+            for (int j = 0; j < edges.Length; j++)
+            {
+                int u = edges[j][0];
+                int v = edges[j][1];
+                double prob = succProb[j];
+
+                if (maxProb[u] * prob > maxProb[v])
+                {
+                    maxProb[v] = maxProb[u] * prob;
+                    updated = true;
+                }
+                if (maxProb[v] * prob > maxProb[u])
+                {
+                    maxProb[u] = maxProb[v] * prob;
+                    updated = true;
+                }
+            }
+            if (!updated) break;
+        }
+
+        return maxProb[end_node];
+    }
+}
