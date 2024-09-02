b7257eb68d5b3d6c3dd90a117c263ba203758e71
Muhammad Qasim <qasimcheema30@gmail.com>
2024-09-02 14:36:00 +0500

    -Added New Problem Solution.

---
+++ FindtheStudentthatWillReplacetheChalk.cs
@@ -0,0 +1,25 @@
+namespace LeetCode.FindtheStudentthatWillReplacetheChalk;
+public class Solution
+{
+    public int ChalkReplacer(int[] chalk, int k)
+    {
+        long totalChalkNeeded = 0;
+        foreach (int studentChalkUse in chalk)
+        {
+            totalChalkNeeded += studentChalkUse;
+        }
+
+        int remainingChalk = (int)(k % totalChalkNeeded);
+
+        for (int studentIndex = 0; studentIndex < chalk.Length; studentIndex++)
+        {
+            if (remainingChalk < chalk[studentIndex])
+            {
+                return studentIndex;
+            }
+            remainingChalk -= chalk[studentIndex];
+        }
+
+        return 0;
+    }
+}
