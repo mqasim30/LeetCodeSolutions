42018bb27ff7a44d2d3eba987673c6201848892c
Muhammad Qasim <qasimcheema30@gmail.com>
2024-09-11 21:53:51 +0500

    -Added New Problem Solution.

---
+++ MinimumBitFlipstoConvertNumber.cs
@@ -0,0 +1,20 @@
+namespace LeetCode.MinimumBitFlipstoConvertNumber;
+
+public class Solution
+{
+    public int MinBitFlips(int start, int goal)
+    {
+        int flips = 0;
+
+        while (!(start == 0) || !(goal == 0))
+        {
+            if ((start % 2) != (goal % 2))
+                flips++;
+
+            start /= 2;
+            goal /= 2;
+        }
+
+        return flips;
+    }
+}
