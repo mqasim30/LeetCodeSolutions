4ee4c161d59cf1a0e1fb1ff8ab9d57bd6577be67
Muhammad Qasim <qasimcheema30@gmail.com>
2024-12-03 20:37:00 +0500

    -Added New Problem Solution.

---
+++ AddingSpacestoaString.cs
@@ -0,0 +1,27 @@
+namespace LeetCode.AddingSpacestoaString;
+
+public class Solution
+{
+    public string AddSpaces(string s, int[] spaces)
+    {
+        char[] result = new char[s.Length + spaces.Length];
+        int writePos = 0;
+        int readPos = 0;
+
+        foreach (int spacePos in spaces)
+        {
+            while (readPos < spacePos)
+            {
+                result[writePos++] = s[readPos++];
+            }
+            result[writePos++] = ' ';
+        }
+
+        while (readPos < s.Length)
+        {
+            result[writePos++] = s[readPos++];
+        }
+
+        return new string(result);
+    }
+}
