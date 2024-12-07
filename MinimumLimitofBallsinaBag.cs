d7495a264e5b908a4304d27ce77d3687be47e467
Muhammad Qasim <qasimcheema30@gmail.com>
2024-12-07 17:40:39 +0500

    -Added New Problem Solution.

---
+++ MinimumLimitofBallsinaBag.cs
@@ -0,0 +1,17 @@
+namespace LeetCode.MinimumLimitofBallsinaBag;
+
+public class Solution
+{
+    public int MinimumSize(int[] nums, int maxOps)
+    {
+        int low = 1, high = nums.Max();
+        while (low < high)
+        {
+            int mid = (low + high) / 2;
+            int ops = nums.Sum(n => (n - 1) / mid);
+            if (ops <= maxOps) high = mid;
+            else low = mid + 1;
+        }
+        return high;
+    }
+}
