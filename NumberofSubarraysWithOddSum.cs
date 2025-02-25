2c5e36c3e7ba88f79d03ce07628ce5ac2de901b2
Muhammad Qasim <qasimcheema30@gmail.com>
2025-02-25 14:21:54 +0500

    -Added New Problem Solution.

---
+++ NumberofSubarraysWithOddSum.cs
@@ -0,0 +1,12 @@
+namespace LeetCode.NumberofSubarraysWithOddSum;
+
+public class Solution
+{
+    public int NumOfSubarrays(int[] arr)
+    {
+        var sum = 0L;
+        var odd = arr.Sum(n => (sum += n) % 2);
+        var res = odd * (arr.Length - odd + 1);
+        return (int)(res % 1_000_000_007);
+    }
+}
