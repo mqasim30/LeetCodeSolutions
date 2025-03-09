972cca0ebbcc7ce04b023db32beafd9c1cc4e249
Muhammad Qasim <qasimcheema30@gmail.com>
2025-03-09 18:34:08 +0500

    -Added New Problem Solution.

---
+++ AlternatingGroupsII.cs
@@ -0,0 +1,46 @@
+namespace LeetCode.AlternatingGroupsII;
+public class Solution
+{
+    public int NumberOfAlternatingGroups(int[] colors, int k)
+    {
+        int length = colors.Length;
+        int result = 0;
+        int alternatingElementsCount = 1;
+        int lastColor = colors[0];
+
+        for (int index = 1; index < length; index++)
+        {
+            if (colors[index] == lastColor)
+            {
+                alternatingElementsCount = 1;
+                lastColor = colors[index];
+                continue;
+            }
+            alternatingElementsCount++;
+
+            if (alternatingElementsCount >= k)
+            {
+                result++;
+            }
+
+            lastColor = colors[index];
+        }
+
+        for (int index = 0; index < k - 1; index++)
+        {
+            if (colors[index] == lastColor)
+                break;
+
+            alternatingElementsCount++;
+
+            if (alternatingElementsCount >= k)
+            {
+                result++;
+            }
+
+            lastColor = colors[index];
+        }
+
+        return result;
+    }
+}
