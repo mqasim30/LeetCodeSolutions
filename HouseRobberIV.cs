c9db02224398db9863c940ff0c1ea974a7ce161e
Muhammad Qasim <qasimcheema30@gmail.com>
2025-03-15 12:15:13 +0500

    -Added New Problem Solution.

---
+++ HouseRobberIV.cs
@@ -0,0 +1,47 @@
+namespace LeetCode.HouseRobberIV;
+
+public class Solution
+{
+    public int MinCapability(int[] nums, int k)
+    {
+        int left = int.MaxValue, right = int.MinValue;
+
+        foreach (int num in nums)
+        {
+            left = Math.Min(left, num);
+            right = Math.Max(right, num);
+        }
+
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
+            if (CanRobAtLeastK(nums, k, mid))
+            {
+                right = mid;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+    private bool CanRobAtLeastK(int[] nums, int k, int cap)
+    {
+        int count = 0;
+        int i = 0;
+
+        while (i < nums.Length)
+        {
+            if (nums[i] <= cap)
+            {
+                count++;
+                i++;
+            }
+            i++;
+        }
+
+        return count >= k;
+    }
+}
