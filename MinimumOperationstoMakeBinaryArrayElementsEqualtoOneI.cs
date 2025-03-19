bd55409ab49b936d294062ad7264c9c950e871e0
Muhammad Qasim <qasimcheema30@gmail.com>
2025-03-19 20:28:00 +0500

    -Added New Problem Solution.

--- MinimumOperationstoMakeBinaryArrayElementsEqualtoOneI.cs
+++ MinimumOperationstoMakeBinaryArrayElementsEqualtoOneI.cs
@@ -24,23 +24,3 @@ public class Solution
             return result;
     }
 }
-
-// public class Program
-// {
-//     public static void Main(string[] args)
-//     {
-//         Solution solution = new Solution();
-
-//         int[] nums1 = { 0, 0, 0, 1 };
-//         int[] nums2 = { 1, 0, 1, 0, 0 };
-//         int[] nums3 = { 1, 1, 1 };
-//         int[] nums4 = { 0, 1, 0, 1, 0, 1, 0, 1 };
-//         int[] nums5 = { 1, 0, 0, 1, 1, 0, 1, 1, 1 };
-
-//         Console.WriteLine(solution.MinOperations(nums1)); // Output: 1
-//         Console.WriteLine(solution.MinOperations(nums2)); // Output: -1
-//         Console.WriteLine(solution.MinOperations(nums3)); // Output: 0
-//         Console.WriteLine(solution.MinOperations(nums4)); // Output: -1
-//         Console.WriteLine(solution.MinOperations(nums5)); // Output: -1
-//     }
-// }
