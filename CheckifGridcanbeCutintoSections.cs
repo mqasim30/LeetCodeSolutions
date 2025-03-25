8e0bb48563f99dca248b51bd88c544ca1eb0e883
Muhammad Qasim <qasimcheema30@gmail.com>
2025-03-25 16:17:00 +0500

    -Added New Problem Solution.

---
+++ CheckifGridcanbeCutintoSections.cs
@@ -0,0 +1,30 @@
+namespace LeetCode.CheckifGridcanbeCutintoSections;
+
+public class Solution
+{
+    public bool CheckValidCuts(int n, int[][] rectangles)
+    {
+        return CheckCuts(rectangles, 0) || CheckCuts(rectangles, 1);
+    }
+    private bool CheckCuts(int[][] rectangles, int dim)
+    {
+        Array.Sort(rectangles, (a, b) => a[dim].CompareTo(b[dim]));
+
+        int gapCount = 0;
+        int furthestEnd = rectangles[0][dim + 2];
+
+        for (int i = 1; i < rectangles.Length; i++)
+        {
+            int[] rect = rectangles[i];
+
+            if (furthestEnd <= rect[dim])
+            {
+                gapCount++;
+            }
+
+            furthestEnd = Math.Max(furthestEnd, rect[dim + 2]);
+        }
+
+        return gapCount >= 2;
+    }
+}
