1d51a56abed837c691cab1fe78f34dd305873ee8
Muhammad Qasim <qasimcheema30@gmail.com>
2025-04-05 12:51:53 +0500

    -Added New Problem Solution.

--- SumofAllSubsetXORTotals.cs
+++ SumofAllSubsetXORTotals.cs
@@ -1,28 +1,16 @@
-using System.Globalization;
-
-namespace LeetCode.SumofAllSubsetXORTotals
+namespace LeetCode.SumofAllSubsetXORTotals;
+public class Solution
 {
-
-    public class Solution
+    private int DFS(int i, int result, int length, int[] nums)
     {
-        private int DFS(int i, int result, int length, int[] nums)
+        if (i == length)
         {
-            if (i == length)
-            {
-                return result;
-            }
-            return DFS(i + 1, result ^ nums[i], length, nums) + DFS(i + 1, result, length, nums);
+            return result;
         }
-        public int SubsetXORSum(int[] nums)
-        {
-            return DFS(0, 0, nums.Length, nums);
-        }
+        return DFS(i + 1, result ^ nums[i], length, nums) + DFS(i + 1, result, length, nums);
     }
-    // public class Program
-    // {
-    //     public static void Main(string[] args)
-    //     {
-    //         Solution solution = new Solution();
-    //     }
-    // }
+    public int SubsetXORSum(int[] nums)
+    {
+        return DFS(0, 0, nums.Length, nums);
+    }
 }
