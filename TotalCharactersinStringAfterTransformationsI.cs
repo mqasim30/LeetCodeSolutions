500fcb9a96f1f2106fcf65b08e9f7ebbe40bbe9d
Muhammad Qasim <qasimcheema30@gmail.com>
2025-05-13 10:04:00 +0500

    -Added New Problem Solution.

---
+++ TotalCharactersinStringAfterTransformationsI.cs
@@ -0,0 +1,33 @@
+namespace LeetCode.TotalCharactersinStringAfterTransformationsI;
+
+
+public class Solution
+{
+    private const int MOD = 1000000007;
+
+    public int LengthAfterTransformations(string s, int t)
+    {
+        int[] cnt = new int[26];
+        foreach (char ch in s)
+        {
+            ++cnt[ch - 'a'];
+        }
+        for (int round = 0; round < t; ++round)
+        {
+            int[] nxt = new int[26];
+            nxt[0] = cnt[25];
+            nxt[1] = (cnt[25] + cnt[0]) % MOD;
+            for (int i = 2; i < 26; ++i)
+            {
+                nxt[i] = cnt[i - 1];
+            }
+            cnt = nxt;
+        }
+        int ans = 0;
+        for (int i = 0; i < 26; ++i)
+        {
+            ans = (ans + cnt[i]) % MOD;
+        }
+        return ans;
+    }
+}
