9f0da689fb20c1de3a3a2fd9462c06500778d632
Muhammad Qasim <qasimcheema30@gmail.com>
2025-07-07 20:33:36 +0500

    -Added New Problem Solution.

---
+++ MaximumNumberofEventsThatCanBeAttended.cs
@@ -0,0 +1,31 @@
+namespace LeetCode.MaximumNumberofEventsThatCanBeAttended;
+
+public class Solution
+{
+    public int MaxEvents(int[][] events)
+    {
+        Array.Sort(events, (a, b) => a[1] == b[1] ? a[0] - b[0] : a[1] - b[1]);
+
+        bool[] seen = new bool[100001];
+        int count = 0;
+        int lastFound = 0;
+        for (int i = 0; i < events.Length; i++)
+        {
+            int end = events[i][1];
+            int start = (i > 0 && events[i - 1][0] == events[i][0]) ? lastFound : events[i][0];
+
+            for (int j = start; j <= end; j++)
+            {
+                if (!seen[j])
+                {
+                    seen[j] = true;
+                    lastFound = j;
+                    count++;
+                    break;
+                }
+            }
+        }
+
+        return count;
+    }
+}
