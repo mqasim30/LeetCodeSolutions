2781e856184df15805284cf1c4276110e6c26267
Muhammad Qasim <qasimcheema30@gmail.com>
2025-07-29 10:26:28 +0500

    -Added New Problem Solution.

---
+++ CountHillsandValleysinanArray.cs
@@ -0,0 +1,36 @@
+namespace LeetCode.CountHillsandValleysinanArray;
+
+public class Solution
+{
+    public int CountHillValley(int[] nums)
+    {
+        if (nums.Length < 3) return 0;
+        int l = 0;
+        int i = 1;
+        int c = 0;
+        for (int r = 2; r < nums.Length; r++)
+        {
+            if (nums[l] == nums[i])
+            {
+                i++;
+                l++;
+            }
+            else if (nums[r] == nums[i])
+            {
+                continue;
+            }
+            else
+            {
+                if ((nums[l] < nums[i] && nums[i] > nums[r]) || (nums[l] > nums[i] && nums[i] < nums[r]))
+                {
+                    c++;
+                }
+
+                l = r - 1;
+                i = r;
+            }
+        }
+        return c;
+
+    }
+}
