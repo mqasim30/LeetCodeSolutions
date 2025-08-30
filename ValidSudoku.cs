e1f271fd19cf7fd6d867a49e6cb5dd321e4c56dc
Muhammad Qasim <qasimcheema30@gmail.com>
2025-08-30 10:41:32 +0500

    -Added New Problem Solution.

--- ValidSudoku.cs
+++ ValidSudoku.cs
@@ -19,7 +19,8 @@ namespace LeetCode.ValidSudoku
                 return false;
             }
 
-            if(!Check3x3(board)){
+            if (!Check3x3(board))
+            {
                 return false;
             }
             return true;
@@ -54,7 +55,6 @@ namespace LeetCode.ValidSudoku
                         }
                         if (ints.Contains(board[row][col] - '0'))
                         {
-                            Console.WriteLine($"Found Duplicate At ({row},{col})");
                             return false;
                         }
                         else
@@ -83,7 +83,6 @@ namespace LeetCode.ValidSudoku
                     }
                     if (ints.Contains(board[row][col] - '0'))
                     {
-                        Console.WriteLine($"Found Duplicate At ({row},{col})");
                         return false;
                     }
                     else
@@ -109,7 +108,6 @@ namespace LeetCode.ValidSudoku
                     }
                     if (ints.Contains(board[row][col] - '0'))
                     {
-                        Console.WriteLine($"Found Duplicate At ({row},{col})");
                         return false;
                     }
                     else
@@ -121,35 +119,4 @@ namespace LeetCode.ValidSudoku
             return true;
         }
     }
-    // public class Program
-    // {
-    //     public static void Main(string[] args)
-    //     {
-    //         Solution solution = new Solution();
-
-    //         char[][] board =
-    //         [['5','3','.','.','7','.','.','.','.']
-    //         ,['6','.','.','1','9','5','.','.','.']
-    //         ,['.','9','8','.','.','.','.','6','.']
-    //         ,['8','.','.','.','6','.','.','.','3']
-    //         ,['4','.','.','8','.','3','.','.','1']
-    //         ,['7','.','.','.','2','.','.','.','6']
-    //         ,['.','6','.','.','.','.','2','8','.']
-    //         ,['.','.','.','4','1','9','.','.','5']
-    //         ,['.','.','.','.','8','.','.','7','9']];
-
-    //         char[][] board1 =
-    //         [['8','3','.','.','7','.','.','.','.']
-    //         ,['6','.','.','1','9','5','.','.','.']
-    //         ,['.','9','8','.','.','.','.','6','.']
-    //         ,['8','.','.','.','6','.','.','.','3']
-    //         ,['4','.','.','8','.','3','.','.','1']
-    //         ,['7','.','.','.','2','.','.','.','6']
-    //         ,['.','6','.','.','.','.','2','8','.']
-    //         ,['.','.','.','4','1','9','.','.','5']
-    //         ,['.','.','.','.','8','.','.','7','9']];
-
-    //         System.Console.WriteLine(solution.IsValidSudoku(board));
-    //     }
-    // }
 }
