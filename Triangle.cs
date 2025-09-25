13c68a46464f9dc2a455edc2e9c06e30bca03a01
Muhammad Qasim <qasimcheema30@gmail.com>
2025-09-25 10:10:08 +0500

    -Added New Problem Solution.

---
+++ Triangle.cs
@@ -0,0 +1,20 @@
+namespace LeetCode.Triangle;
+
+public class Solution
+{
+    public int MinimumTotal(IList<IList<int>> triangle)
+    {
+        if (triangle.Count == 0) return 0;
+
+        for (int i = 1; i < triangle.Count; i++)
+            for (int j = 0; j <= i; j++)
+                if (j == 0)
+                    triangle[i][j] += triangle[i - 1][j];
+                else if (j == i)
+                    triangle[i][j] += triangle[i - 1][j - 1];
+                else
+                    triangle[i][j] += Math.Min(triangle[i - 1][j], triangle[i - 1][j - 1]);
+
+        return triangle.Last().Min();
+    }
+}
