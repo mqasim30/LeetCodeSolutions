6e103951878724f9409c2d792e1142b5d3dc4871
Muhammad Qasim <qasimcheema30@gmail.com>
2025-09-28 14:32:34 +0500

    -Added New Problem Solution.

---
+++ LargestPerimeterTriangle.cs
@@ -0,0 +1,17 @@
+namespace LeetCode.LargestPerimeterTriangle;
+
+
+public class Solution
+{
+    public int LargestPerimeter(int[] nums)
+    {
+        Array.Sort(nums);
+        for (int i = nums.Length - 3; i >= 0; i--)
+        {
+            if (nums[i] + nums[i + 1] > nums[i + 2])
+                return nums[i] + nums[i + 1] + nums[i + 2];
+        }
+
+        return 0;
+    }
+}
