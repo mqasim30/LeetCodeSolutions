38a8b51b26f169b26798c3d19e990f0228cdb196
Muhammad Qasim <qasimcheema30@gmail.com>
2025-09-30 11:59:43 +0500

    -Added New Problem Solution.

---
+++ FindTriangularSumofanArray.cs
@@ -0,0 +1,19 @@
+namespace LeetCode.FindTriangularSumofanArray;
+
+public class Solution
+{
+    public int TriangularSum(int[] nums)
+    {
+        int n = nums.Length;
+
+        for (int i = 1; i < n; i++)
+        {
+            for (int j = 0; j < n - i; j++)
+            {
+                nums[j] = (nums[j] + nums[j + 1]) % 10;
+            }
+        }
+
+        return nums[0];
+    }
+}
