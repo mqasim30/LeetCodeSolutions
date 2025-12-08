0a6e5a554340dad55783ff1ccd22e0e8dd423b10
Muhammad Qasim <qasimcheema30@gmail.com>
2025-12-08 22:22:06 +0500

    -Added New Problem Solution.

---
+++ CountSquareSumTriples.cs
@@ -0,0 +1,23 @@
+namespace LeetCode.CountSquareSumTriples;
+
+public class Solution
+{
+    public int CountTriples(int n)
+    {
+        int res = 0;
+        // enumerate a and b
+        for (int a = 1; a <= n; ++a)
+        {
+            for (int b = 1; b <= n; ++b)
+            {
+                // determine if it meets the requirements
+                int c = (int)Math.Sqrt(a * a + b * b + 1.0);
+                if (c <= n && c * c == a * a + b * b)
+                {
+                    ++res;
+                }
+            }
+        }
+        return res;
+    }
+}
