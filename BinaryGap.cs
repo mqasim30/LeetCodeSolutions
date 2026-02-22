4cb6793a03ebf54ace5be8b5a9ed7937ec8e59af
Muhammad Qasim <qasimcheema30@gmail.com>
2026-02-22 18:21:47 +0500

    -Added New Problem Solution.

---
+++ BinaryGap.cs
@@ -0,0 +1,21 @@
+namespace LeetCode.BinaryGap;
+
+public class Solution
+{
+    public int BinaryGap(int n)
+    {
+        var last = -1;
+        var result = 0;
+        for (int i = 0; i < 32; i++)
+        {
+            if (((n >> i) & 1) == 1)
+            {
+                if (last >= 0)
+                    result = Math.Max(result, i - last);
+                last = i;
+            }
+        }
+
+        return result;
+    }
+}
